da454cf74ca2804e1a2a516cb70e83cb64d654c6
Matteo Casucci <matteocasucci10@gmail.com>
2020-04-04 17:45:48 +0200

    Aggiungere i file di progetto.

---
+++ ClienttDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienttDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetSum_Click(object sender, RoutedEventArgs e)
+        {
+            double ris = 0;
+            int a = Convert.ToInt32(txtN1.Text);
+            int b = Convert.ToInt32(txtN2.Text);
+            HttpClient client = new HttpClient();
+            string uri = $"https://localhost:44353/api/Operations/GetSum?a=122&b={b}";
+            HttpResponseMessage response = await client.GetAsync(uri);
+            string content = await response.Content.ReadAsStringAsync();
+            ris = JsonConvert.DeserializeObject<int>(content);
+            Dispatcher.Invoke(() => lblRis.Content = ris);
+
+        }
+    }
+}
---
+++ WebDemo/Controllers/OperationsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class OperationsController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public double GetSum(int a , int b)
+        {
+            return a + b;
+        }
+
+        [HttpGet]
+        public double GetSubtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        [HttpGet]
+        public double GetProduct(int a, int b)
+        {
+            return a * b;
+        }
+
+        [HttpGet]
+        public double GetDivision(int a, int b)
+        {
+            return a / b;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
